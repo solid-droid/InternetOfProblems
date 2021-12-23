dc39616fcbc467f09fc0dc9a3d5e26b9eeabc941
Nikhil M Jeby <32586986+solid-droid@users.noreply.github.com>
2021-12-23 20:23:27 +0530

    added openLayers package

--- src/app/screens/home/home.component.ts
+++ src/app/screens/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare const ol:any;
 
 @Component({
   selector: 'app-home',
@@ -6,11 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  map:any;
+  
   constructor() { }
 
  async ngOnInit() {
+    this.createMap();
 
+  }
 
-}
+  createMap() {
+    this.map = new ol.Map({
+      target: 'map',
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM()
+        })
+      ],
+      view: new ol.View({
+        center: ol.proj.fromLonLat([37.41, 8.82]),
+        zoom: 4
+      })
+    });
+  }
 }
