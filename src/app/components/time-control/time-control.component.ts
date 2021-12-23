ebb40b54a02174465d841b8131498347f23a67d5
Nikhil M Jeby <32586986+solid-droid@users.noreply.github.com>
2021-12-23 14:28:29 +0530

    timeControl

--- src/app/components/time-control/time-control.component.ts
+++ src/app/components/time-control/time-control.component.ts
@@ -7,7 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimeControlComponent implements OnInit {
 
-  constructor() { }
+  dateRange: Date[] = this.createDateRange();
+  value: number = this.dateRange[0].getTime();
+  options:any = {
+    stepsArray: this.dateRange.map((date: Date) => {
+      return { value: date.getTime() };
+    }),
+    translate: (value: number, label: any): string => {
+      return new Date(value).toDateString();
+    }
+  };
+
+  createDateRange(): Date[] {
+    const dates: Date[] = [];
+    for (let i: number = 1; i <= 31; i++) {
+      dates.push(new Date(2018, 5, i));
+    }
+    return dates;
+  }
 
   ngOnInit(): void {
   }
