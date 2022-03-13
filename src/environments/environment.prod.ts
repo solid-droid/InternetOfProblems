0bcd535f306c6835ac5fb579b06217fca9e293c7
Nikhil M Jeby <32586986+solid-droid@users.noreply.github.com>
2022-03-13 13:43:48 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   googleLogin : '419386531032-djlv53qm33oriacfu3n8u8jbgl4bk3a9.apps.googleusercontent.com',
-  backendUrl: 'https://internet-of-problems-backend.herokuapp.com/'
+  backendUrl: 'https://internet-of-problemsv2-backend.herokuapp.com/'
 };
